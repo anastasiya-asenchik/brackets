6b7ebb294923a8b97e76a03f28de77e46eaef0ca
Anastasiya <anastasiaasn@gmail.com>
2020-10-06 23:56:58 +0300

    feat: add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let map = new Map();
+    let result = [];
+
+    for (let brackets of bracketsConfig) {
+        map.set(brackets[0], brackets[1]);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === result[result.length - 1]) {
+            result.pop();
+        } else {
+            result.push(map.get(str[i]));
+        }
+    }
+
+    return result.length === 0;
 }
